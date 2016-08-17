7842dc36dca47a7e1b87fc06cc3d26ec95673c6c
sharabassy <abdelrahman.elsharabassy@gmail.com>
2016-08-17 09:09:10 +0200

    added tests for index view

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -13,7 +14,8 @@ class IndexView(generic.ListView):
 	context_object_name='latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()
+								).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	template_name='polls/detail.html'
